6022da522f195f2c77ae2b4c1019f8db6e3df0dc
Jan Krüger <krueger@modell-aachen.de>
2013-02-26 16:38:10 +0100

    Kompatibilität mit neuem JQuery und SWP
    * LiveQuery funktioniert in einigen Versionen nicht mehr mit der alten
      Syntax. Verwende neue Syntax.
    * Sorge dafür, dass es nicht nötig ist, JS-Code direkt in der Seite zu
      rendern.

--- pub/System/ActionTrackerPlugin/atp_src.js
+++ pub/System/ActionTrackerPlugin/atp_src.js
@@ -25,48 +25,50 @@
 	});
     };
 
-    $("select.atp_update").livequery("change", restUpdate);
-    $("input.atp_update").livequery("click", restUpdate);
+    $("select.atp_update").livequery(function() { $(this).change(restUpdate); });
+    $("input.atp_update").livequery(function() { $(this).click(restUpdate); });
 
-    $('a.atp_edit').livequery("click", function(event) {
-	var div = $("#atp_editor");
-	if (!div.length) {
-	    div = $("<div id='atp_editor' title='Edit action'></div>");
-	    $("body").append(div);
-	    div.dialog({autoOpen: false, width: 600});
-	}
-	div.load(this.href,
-		 function(done, status) {
-		     var m = /<!-- ATP_CONFLICT ~(.*?)~(.*?)~(.*?)~(.*?)~ -->/.exec(done, "s");
-		     if (m) {
-			 // Messages are defined in oopsleaseconflict.action.tmpl
-			 var message= /<!-- ATP_CONFLICT_MESSAGE ~(.*?)~(.*?)~(.*?)~(.*?)~(.*?)~(.*?)~ -->/.exec(done, "s");
-			 if (message === null){
-			     message=[
-				  'Could not access ' + m[1] + ', the topic containing this action',
-				  m[2] + ' may still be editing the topic, but their lease expired ' + m[3] + ' ago.',
-				  m[2] + 'has been editing the topic for ' + m[3] + 'and their lease is still active for another ' + m[4],
-				  'To clear the lease, try editing ' + m[1] + ' with the standard text editor.',
-				  'Error when I tried to edit the action',
-				  'Sorry, validation failed, possibly because someone else is already editing the action. Pleas try again in a few minutes.'
-			     ];
-			 }
-			 var ohno = message[1]; //Could not access...containing the action.
-			 if (m[4] == "") {
-			     ohno += message[2]; //...may still be editing the topic....ago.
+    $('a.atp_edit').livequery(function() {
+	$(this).click(function(event) {
+	    var div = $("#atp_editor");
+	    if (!div.length) {
+		div = $("<div id='atp_editor' title='Edit action'></div>");
+		$("body").append(div);
+		div.dialog({autoOpen: false, width: 600});
+	    }
+	    div.load(this.href,
+		     function(done, status) {
+			 var m = /<!-- ATP_CONFLICT ~(.*?)~(.*?)~(.*?)~(.*?)~ -->/.exec(done, "s");
+			 if (m) {
+			     // Messages are defined in oopsleaseconflict.action.tmpl
+			     var message= /<!-- ATP_CONFLICT_MESSAGE ~(.*?)~(.*?)~(.*?)~(.*?)~(.*?)~(.*?)~ -->/.exec(done, "s");
+			     if (message === null){
+				 message=[
+				      'Could not access ' + m[1] + ', the topic containing this action',
+				      m[2] + ' may still be editing the topic, but their lease expired ' + m[3] + ' ago.',
+				      m[2] + 'has been editing the topic for ' + m[3] + 'and their lease is still active for another ' + m[4],
+				      'To clear the lease, try editing ' + m[1] + ' with the standard text editor.',
+				      'Error when I tried to edit the action',
+				      'Sorry, validation failed, possibly because someone else is already editing the action. Pleas try again in a few minutes.'
+				 ];
+			     }
+			     var ohno = message[1]; //Could not access...containing the action.
+			     if (m[4] == "") {
+				 ohno += message[2]; //...may still be editing the topic....ago.
+			     } else {
+				 ohno += message[3]; //...has been editing the topic...
+			     }
+			     ohno += message[4]; //To clear the lease...
+			     div.html(ohno);
+			     div.dialog("open");
+			 } else if (status == "error") {
+			     alert(message[5]); //Error when I tried...
 			 } else {
-			     ohno += message[3]; //...has been editing the topic...
+			     div.dialog("open");
 			 }
-			 ohno += message[4]; //To clear the lease...
-			 div.html(ohno);
-			 div.dialog("open");
-		     } else if (status == "error") {
-			 alert(message[5]); //Error when I tried...
-		     } else {
-			 div.dialog("open");
-		     }
-		 });
-	return false;
+		     });
+	    return false;
+	});
     });
 
     $('#atp_editor input[type="submit"]').livequery(function() {
@@ -76,8 +78,8 @@
 	});
     });
 
-    $("#atp_editor form[name='loginform']").livequery("submit",
-	function() {
+    $("#atp_editor form[name='loginform']").livequery(function() {
+	$(this).submit(function() {
 	    var form = $(this);
 	    $.ajax({
 		url: form.attr("action"),
@@ -94,4 +96,13 @@
 	    });
 	    return false;
 	});
+    });
+
+    /* calendar for due date */
+    $('#atp_editor input[name="calendar"]').livequery(function() {
+	var cal = $(this);
+	$(this).click(function() {
+	    return showCalendar(cal.prev().attr('id'), '%d %b %Y');
+	});
+    });
 })(jQuery); 
